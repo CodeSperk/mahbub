4085a759875fd73227ee674678bea52357b88a98
mahbubur Rahman <inbx.mahbub@gmail.com>
2024-12-26 22:00:06 +0600

    added footer

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,15 +5,19 @@ import LeftContact from "@/Pages/Contact/__LeftContact";
 
 const ContactPage = () => {
   return (
-    <div className="page-bg min-h-screen">
+    <div className="page-bg min-h-screen flex flex-col justify-between">
       <PageTitle bg="Contact" title1="Get In" title2="touch" />
 
       <section>
-        <div className="mt-12 md:mt-16 lg:mt-20 flex flex-col-reverse md:flex-row justify-center gap-12 lg:gap-16 text-white">
+        <div className="my-12 md:my-16 lg:my-20 flex flex-col-reverse md:flex-row justify-center gap-12 lg:gap-16 text-white">
           <LeftContact />
           <ContactForm />
         </div>
       </section>
+
+      <footer className="bg-[#fefef5] h-[50px] flex justify-center items-center">
+        <div className="text-xl"> All right reserved © Mahbubur Rahman, 2024</div>
+      </footer>
     </div>
   );
 };
